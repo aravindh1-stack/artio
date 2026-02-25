ccd879179d717635d37ed85380d00676f1f4efe4
aravindh1-stack <aravindhofficiallinks@gmail.com>
2026-02-25 08:54:41 +0530

    neon final

--- server/db.js
+++ server/db.js
@@ -1,10 +1,9 @@
-// Neon Postgres connection using 'pg' library
 const { Pool } = require('pg');
 require('dotenv').config();
 
 const pool = new Pool({
   connectionString: process.env.DATABASE_URL,
-  ssl: { rejectUnauthorized: false },
+  ssl: process.env.DATABASE_SSL === 'true' ? { rejectUnauthorized: false } : false,
 });
 
 module.exports = { pool };
